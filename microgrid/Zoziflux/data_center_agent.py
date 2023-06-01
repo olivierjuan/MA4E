59f8b1313531e9e2459abe4937c48fe70d5c7981
largo <largo.gioana@gmail.com>
2023-06-01 19:09:30 +0200

    commit

--- microgrid/Zoziflux/data_center_agent.py
+++ microgrid/Zoziflux/data_center_agent.py
@@ -5,6 +5,8 @@ import numpy as np
 from scipy.stats import bernoulli
 
 
+
+
 def bernoulli_param(delta_price: float, delta_price_max: float, delta_price_min: float):
     """
     Calculate the parameter of a Bernouilli law, based on price difference btw Hot-Water and elec. versus min. and max.
@@ -42,7 +44,7 @@ class DataCenterAgent:
                       consumption_forecast: np.ndarray,     # in R+^nbr_future_time_slots
                       hotwater_price_forecast: np.ndarray   # in R+^nbr_future_time_slots
                       ) -> np.ndarray:  # in [0,1]^nbr_future_time_slots (heat pump activation profile)
-        baseline_decision = self.take_baseline_decision(manager_signal=manager_signal,
+        baseline_decision = self.take_my_decision(manager_signal=manager_signal,
                                                         hotwater_price_forecast=hotwater_price_forecast)
         # use format and feasibility "checker"
         check_msg = self.check_decision(load_profile=baseline_decision, consumption_forecast=consumption_forecast)
@@ -71,9 +73,35 @@ class DataCenterAgent:
 
         return baseline_decision
 
+    def take_my_decision(self,
+                           manager_signal: np.ndarray,  # in R^nbr_future_time_slots
+                           hotwater_price_forecast: np.ndarray   # in R+^nbr_future_time_slots
+                           ) -> np.ndarray:
+        baseline_decision = np.zeros(self.nbr_future_time_slots)
+        for t in range(self.nbr_future_time_slots):
+                dt = self.delta_t.total_seconds() / 3600)
+                if(consumption_forecast[t]*(hotwater_price_forecast[t]*1.2/0.2*1.25 -
+                                            manager_signal[t]*5.0/(4*0.5*0.2)) >= 0):
+                #on veut minimiser le produit a(t)*f(t) donc si f(t) < 0, on prend a(t) maximal =1
+                a = 10*0.2*4/(1.2*5*consumption_forecast[t])
+                baseline_decision[t] = a*consumption_forecast[t]*self.COP_CS/(self.COP_HP-1)*self.delta_t/3600)
+                #inversement ici, ce n’est pas rentable donc on n’échange ie on laisse la valeur nulle
+    return baseline_decision
+
+
+
+
+#test de la fonction take_my_decision
+    def benef(self,
+                manager_signal: np.ndarray,  # in R^nbr_future_time_slots
+              consumption_forecast: np.ndarray,  # in R+^nbr_future_time_slots
+              hotwater_price_forecast: np.ndarray  # in R+^nbr_future_time_slots
+              ) -> float:
+        l_HP = self.take_my_decision(manager_signal=manager_signal, consumption_forecast=consumption_forecast, hotwater_price_forecast=hotwater_price_forecast)
+        benef = np.sum(-(1 + 1 / (4 * 0.5)) * consumption_forecast + ( - self.COP_HP * (self.delta_t / 3600) * hotwater_price_forecast + manager_signal) * l_HP)
+        return benef
     def check_decision(self, load_profile, consumption_forecast):
-        check_msg, check_score = check_data_center_feasibility(data_center_env=self.env, load_profile=load_profile,
-                                                               it_load_profile=consumption_forecast)
+        check_msg, check_score = check_data_center_feasibility(data_center_env=self.env, load_profile=load_profile, it_load_profile=consumption_forecast)
         return check_msg
 
 
